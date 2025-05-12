f72ccab906f076d772ef8fe419ac8af3cc46b64f
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-05-12 21:52:49 +0200

    Remove obsolete code, get button element
    Clear unnecessary code, get 'view my work' button and add an event listener to it that causes it to scroll the page to the projects section

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-let rateEl = document.getElementById("rate")
-console.log(rateEl)
+let viewEl = document.getElementById("view-work");
 
-let rating = 0;
-
-function increment(){
-    if(rating != 10){
-    rating = rating + 1;
-    rateEl.innerText = `${rating}/10`
-    }
-    else{
-        rateEl.innerText = `${rating}/10 (Cannot higher than 10)`
-    }
-}
-
-function decrement(){
-    if(rating != 0){
-    rating = rating - 1;
-    rateEl.innerText = `${rating}/10`
-    }
-    else{
-        rateEl.innerText = `${rating}/10 (Cannot lower than 0)`
-    }
-}
+viewEl.addEventListener('click',()=>{
+    document.getElementById('projects').scrollIntoView();
+})
