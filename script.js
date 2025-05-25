20c2c4ddfaed8527d795b48ed5f1864ffb0050b8
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-05-25 23:31:27 +0200

    Add toggle function for responsive navbar

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let viewEl = document.getElementById("view-work");
+const viewEl = document.getElementById("view-work");
 
 viewEl.addEventListener('click',()=>{
     document.getElementById('projects').scrollIntoView();
 })
+
+const navbar = document.getElementById("navbar");
+const nameHeading = document.getElementById("name");
+
+/* Toggle between adding and removing the "responsive" class when icon is clicked*/
+function toggle(){
+    if(navbar.className == ""){
+        navbar.className = "responsive";
+        nameHeading.style.display = "none";
+    }else{
+        navbar.className = "";
+        nameHeading.style.display = "block";
+    }
+    
+}
+
